6f500e77b5d98ed3af6a1daea33698ffd70a41a5
arnobarzan <arno.barzan@pxl.be>
2025-10-12 22:22:48 +0200

    .NET9

--- SolarSystemApp/App.xaml.cs
+++ SolarSystemApp/App.xaml.cs
@@ -1,11 +1,16 @@
-﻿namespace SolarSystemApp;
+﻿
+namespace SolarSystemApp;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+
 }
